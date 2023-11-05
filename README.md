a3735cf35fbaf4387309f2d01451268a53a4c1a2
BleedTheFreak <tayayassine6@live.fr>
2023-11-05 23:16:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Calculator Package
+
+    This is a simple calculator package for basic arithmetic operations.
+
+    ## Installation
+
+    You can install the package from GitHub repository using pip:
+
+    ```
+    pip install git+https://github.com/yourusername/calculator_package.git
+    ```
+
+    ## Usage
+
+    Here is how you can use the calculator:
+
+    ```python
+    from calculator import add, subtract, multiply, divide
+
+    print(add(2, 3))  # Output: 5
+    print(subtract(5, 2))  # Output: 3
+    print(multiply(3, 4))  # Output: 12
+    print(divide(8, 2))  # Output: 4
+    ```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+    setup(
+        name='calculator_package',
+        version='0.1.0',
+        packages=find_packages(),
+        description='A simple calculator package',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        author='Your Name',
+        author_email='your.email@example.com',
+        license='MIT',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Education',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.9',
+        ],
+        python_requires='>=3.6',
+    )
